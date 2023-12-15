ef1eb92f874c87b0c957d72fc3230ea4fc61a901
Nick Anderson <nickbanderson@bellflight.com>
2023-12-15 13:04:23 -0600

    wip: more progress

--- perspective-sets.el
+++ perspective-sets.el
@@ -4,44 +4,36 @@
 
 ;; Perspective sets (AKA psets) organize perspectives (AKA persps) together.
 ;;
-;; Perspective names (in the context of perspective.el) are the concatenation of the set name,
-;; '/' (or 'psets-separator-of-set-and-persp), and the set entry's name. This is done to ensure
-;; that perspective names are unique.
-;;
-;; Instead of using persp-* functions to interact with perspective.el directly, use psets-* functions which wrap them.
+;; Perspective.el already manages sets of persps (and related data) through frame
+;; parameters so you can have independent persps per frame. Perspective-sets manages
+;; these frame parameters to allow smooth pset swappage within one frame.
 
 ;;; Code:
 
-(defcustom psets-default-set-name "main"
+(defcustom psets-default-pset-name "main"
   "Name of the default pset."
   :type 'string
   :group 'perspective-sets)
 
-(defcustom psets-separator-of-set-and-persp "/"
-  "String separating the string of a pset and a persp (i.e. \"/\" -> my-pset/my-persp)"
-  :type 'string
-  :group 'perspective-sets)
-
-(defun psets-full-persp-name (pset-name persp-name)
-  "Defines how to map the name of a persp and its pset into a true, unique persp name."
-  (concat pset-name psets-separator-of-set-and-persp persp-name))
+(defvar psets-active-pset-name psets-default-pset-name
+  "The name (string) of the currently active pset.")
 
 (defun psets-active-pset ()
-  "Return pset name of active persp."
-  (let* ((full-persp-name (persp-name (persp-curr)))
-        (pset-name (substring full-persp-name 0 (string-match psets-separator-of-set-and-persp full-persp-name))))
-    pset-name))
+  "Returns the currently active pset by accessing frame parameters used by perspective.el."
+  ;; WAIT use cl-defstruct! tagged vector!!! lol
+  (list
+   (frame-parameter nil 'persp--hash)
+   (frame-parameter nil 'persp--curr)
+   (frame-parameter nil 'persp--last)
+   (frame-parameter nil 'persp--recursive)
+   (frame-parameter nil 'persp--modestring)))
+     ;; '((persp--hash) (persp--curr) (persp--last) (persp--recursive) (persp--modestring)))
 
 (defvar psets--psets
-  (let ((sets (make-hash-table :test 'equal)))
-    (puthash psets-default-set-name (list persp-initial-frame-name) sets)
-    sets)
-  "Hash table mapping psets to their contained persps.
-(key: value) = (pset name string: list of persp name strings belonging to that set)
-Example: (\"juicer-set\": '(\"p1\" \"p2\"))")
-
-  ;; (mapcar #'make-persp (psets-full-persp-names pset-name persp-names))
-  ;; (mapcar (lambda (full-persp-name) (make-persp :name full-persp-name)) (psets-full-persp-names pset-name persp-names))
+  (let ((psets (make-hash-table :test 'equal)))
+    (puthash psets-default-pset-name (psets-active-pset) psets)
+    psets)
+  "Hash table mapping each pset name (string) to its associated pset.")
 
 (defun psets-create-pset (pset-name &optional persp-names)
   "Create a new pset PSET-NAME with optional PERSP-NAMES.
@@ -54,7 +46,7 @@ Automatically prepends PSET-NAME before each of PERSP-NAMES to promote uniquenes
 
 (defun psets-create-persp (persp-name &optional pset-name)
   "Create persp PERSP-NAME in pset PSET-NAME (default: current pset)."
-  (unless pset-name (setq pset-name (psets-active-pset)))
+  (unless pset-name (setq pset-name psets-active-pset-name))
   (cl-assert (and (stringp persp-name) (stringp pset-name)))
   ;; TODO this prompts for a name instead of accepting my argument
   (make-persp (psets-full-persp-name pset-name persp-name))
@@ -62,7 +54,7 @@ Automatically prepends PSET-NAME before each of PERSP-NAMES to promote uniquenes
 
 (defun psets-kill-persp (full-persp-name &optional pset-name)
   "Remove persp PERSP-NAME in pset PSET-NAME (default: current pset)."
-  (unless pset-name (setq pset-name (psets-active-pset)))
+  (unless pset-name (setq pset-name psets-active-pset-name))
   (cl-assert (and (stringp persp-name) (stringp pset-name)))
   (persp-kill persp-name)
   (unless (member persp-name persp-names))
